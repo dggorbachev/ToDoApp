a81a6f3446fc97a2a4de9edee8d79293dc823c30
dggorbachev <dggorbachev@edu.hse.ru>
2022-05-24 21:28:12 +0300

    added navcomponents

---
+++ app/src/main/java/com/dggorbachev/todoapp/features/tasks_screen/ui/TasksFragment.kt
@@ -0,0 +1,7 @@
+package com.dggorbachev.todoapp.features.tasks_screen.ui
+
+import androidx.fragment.app.Fragment
+import com.dggorbachev.todoapp.R
+
+class TasksFragment:Fragment(R.layout.fragment_tasks) {
+}
